62b687cfc8e7ac9569ded88a7fc14d1bcdae64ad
47karimasif <karimasif98@gmail.com>
2021-10-09 23:18:01 +0530

    testing for auth service using jest

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,6 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
 
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-const app = express();
-app.set("trust proxy", true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all("*", async (req, res) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -38,18 +8,14 @@ const start = async () => {
   }
 
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
     console.log("Connected to MongoDb");
   } catch (err) {
     console.error(err);
   }
 
   app.listen(3000, () => {
-    console.log("Listening on port 3000!");
+    console.log("Listening on port 3000!!!!!!!!");
   });
 };
 
