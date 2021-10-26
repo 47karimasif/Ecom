7e1e1a61375ba7450c5184597e664c149a91b933
47karimasif <karimasif98@gmail.com>
2021-10-26 22:05:46 +0530

    added listener to ticket for order-create & updated as well for changing ticket version in order database publising events from ticked listeneres

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
+import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -35,6 +37,9 @@ const start = async () => {
 
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDb");
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
   } catch (err) {
     console.error(err);
   }
